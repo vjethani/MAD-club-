d2d25d618ad541d855499aaec64a47b3c5b05ed3
vjethani <jethani.varsha@gmail.com>
2019-04-30 00:03:10 +0530

    Registration along with Login screen and profile Activity screen

---
+++ app/src/main/java/com/example/registration/ProfileActivity.java
@@ -0,0 +1,50 @@
+package com.example.registration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private TextView textViewUserEmail;
+    private Button buttonlogout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        textViewUserEmail=(TextView) findViewById(R.id.textViewUserEmail);
+        buttonlogout=(Button) findViewById(R.id.buttonlogout);
+        buttonlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+
+
+            }
+        });
+        if(firebaseAuth.getCurrentUser()==null)
+        {
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+        }
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+
+
+        textViewUserEmail.setText("WELCOME "+ user.getEmail());
+
+
+    }
+
+
+}
